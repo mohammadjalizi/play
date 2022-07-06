05925766787fd6d1777b899c17d8ed7e300b7ee2
mohammadjalizi <mjalizi1376@gmail.com>
2022-07-06 21:28:10 +0800

    project tailwind

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,8 @@
 const Menu=document.getElementById("menu")
 const scroolm=document.getElementById("btn")
 const Navbar=document.querySelector(".menubar")
-console.log(Navbar)
+const  navlinks=document.querySelectorAll(".nav-link")
+console.log(navlinks)
 Menu.addEventListener("click",()=>{
 Navbar.classList.toggle("active")
 
@@ -19,4 +20,11 @@ scroolm.classList.add("show")
 scroolm.addEventListener("click",()=>{
 window.scrollTo({top:0,behavior:"smooth"})
     
+})
+navlinks.forEach((navlink)=>{
+navlink.addEventListener("click",()=>{
+
+    Navbar.classList.toggle("active")
+})
+
 })
