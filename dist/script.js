8633bd2e0084197b33dbb8712e4340b2c38470e7
mohammadjalizi <mjalizi1376@gmail.com>
2023-01-01 22:39:43 +0330

    project

--- dist/script.js
+++ dist/script.js
@@ -2,7 +2,7 @@ const Menu=document.getElementById("menu")
 const scroolm=document.getElementById("btn")
 const Navbar=document.querySelector(".menubar")
 const  navlinks=document.querySelectorAll(".nav-link")
-AOS.init();
+
 console.log(navlinks)
 Menu.addEventListener("click",()=>{
 Navbar.classList.toggle("active")
